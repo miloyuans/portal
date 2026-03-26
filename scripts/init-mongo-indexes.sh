6854be1ef0f6224ab76c9289d07cb5ec7cb142c0
milo <miloyuans@gmail.com>
2026-03-26 15:19:31 +0400

    up good code

--- scripts/init-mongo-indexes.sh
+++ scripts/init-mongo-indexes.sh
@@ -13,4 +13,12 @@ fi
 : "${MONGO_DB:=portal}"
 
 echo "Initializing Mongo indexes on ${MONGO_URI}/${MONGO_DB}"
-MONGO_DB="${MONGO_DB}" mongosh "${MONGO_URI}/${MONGO_DB}" "${ROOT_DIR}/scripts/init-mongo-indexes.js"
+
+if command -v mongosh >/dev/null 2>&1; then
+  MONGO_DB="${MONGO_DB}" mongosh "${MONGO_URI}/${MONGO_DB}" "${ROOT_DIR}/scripts/init-mongo-indexes.js"
+  exit 0
+fi
+
+echo "Local mongosh not found, falling back to the mongo container"
+
+docker compose exec -e MONGO_DB="${MONGO_DB}" -T mongo sh -lc 'mongosh "mongodb://localhost:27017/${MONGO_DB}"' < "${ROOT_DIR}/scripts/init-mongo-indexes.js"
